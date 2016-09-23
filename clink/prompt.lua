6f465abc0fec406e73f38708523aa51473ec9923
ayapi <colors.aya@gmail.com>
2016-09-24 05:28:03 +0900

    [win][cmd] speed up prompt rendering

--- clink/prompt.lua
+++ clink/prompt.lua
@@ -1,41 +1,51 @@
 local color = require('color');
 
 function ayapi_prompt_filter()
-  local name = os.getenv("USERNAME") .. '@' .. os.getenv("COMPUTERNAME")
-  local datetime = os.date("%Y-%m-%d %H:%M:%S")
-  local home = os.getenv("USERPROFILE")
-  local root = os.getenv("HOMEDRIVE") .. "\\"
-  local pwd = io.popen("cd"):read('*l')
-  pwd = string.gsub(pwd, home, '~', 1)
-  pwd = string.gsub(pwd, root, '/', 1)
-  pwd = string.gsub(pwd, '\\', '/')
+  local p = io.popen("tput cols & cd & echo %USERNAME%@%COMPUTERNAME%^|%USERPROFILE%^|%HOMEDRIVE% & git branch 2>nul")
+  
+  local cols = p:read('*l') - 1
+  local cwd = p:read('*l')
+  local envs_str = p:read('*l')
+  local envs = {}
+  for v in string.gmatch(envs_str, '[^|]+') do
+    table.insert(envs, v)
+  end
+  
+  local name = envs[1]
+  local datetime = os.date("%y-%m-%d %H:%M:%S")
+  local home = envs[2]
+  local root = envs[3] .. "\\"
+  
+  cwd = string.gsub(cwd, home, '~', 1)
+  cwd = string.gsub(cwd, root, '/', 1)
+  cwd = string.gsub(cwd, '\\', '/')
+  
   local branch = ""
   
-  for line in io.popen("git branch 2>nul"):lines() do
+  for line in p:lines() do
     branch = line:match("%* (.+)$")
     if branch then
       break
     end
   end
   
-  local cols = io.popen("tput cols"):read('*l') - 1
   local used_cols = 2 + string.len(name) + 2 + string.len(datetime) + 2
   if (branch ~= '') then
     used_cols = used_cols + string.len(branch) + 2
   end
   local remain_cols = cols - used_cols
   if (remain_cols > 2) then
-    local pwd_len = string.len(pwd)
-    if (pwd_len > remain_cols) then
-      pwd = '..' .. string.sub(pwd, (remain_cols - 2) * -1)
+    local cwd_len = string.len(cwd)
+    if (cwd_len > remain_cols) then
+      cwd = '..' .. string.sub(cwd, (remain_cols - 2) * -1)
     else
-      pwd = pwd .. string.rep(' ', remain_cols - pwd_len)
+      cwd = cwd .. string.rep(' ', remain_cols - cwd_len)
     end
   end
   
   local cells = {
     color.color_text(' ' .. name .. ' ', color.WHITE ,color.MAGENTA),
-    color.color_text(' ' .. pwd .. ' ', color.WHITE, color.BLACK),
+    color.color_text(' ' .. cwd .. ' ', color.WHITE, color.BLACK),
     color.color_text(' ' .. datetime .. ' ', color.BLACK ,color.WHITE),
     "\n > "
   }
